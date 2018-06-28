3907f1625548b070f97afff4ce1644654042eb94
Nilay Singh <nilayy1234@gmail.com>
2018-06-28 13:57:39 +0530

    added

--- src/app/data/districts.ts
+++ src/app/data/districts.ts
@@ -27,7 +27,7 @@ export const Districts:any = [
     "Nawada",
     "Patna",
     "Purnia",
-    "Rohats",
+    "Rohtas",
     "Saharsa",
     "Samastipur",
     "Saran",
--- src/app/services/svg.service.ts
+++ src/app/services/svg.service.ts
@@ -5,22 +5,22 @@ import { iif } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { LoadingModule } from 'ngx-loading';
 import { NgxSpinnerService } from 'ngx-spinner';
-
+import{Functions} from '../data/f'
+// var myValidator = new Functions();
+// declare var CanvasJS:any;
+ let f = new Functions()
 declare var CanvasJS:any;
 @Injectable({
   providedIn: 'root'
 })
 export class SvgService {
   public loading = false;
-
   constructor(private http: HttpClient,private spinner: NgxSpinnerService) { }
   // private apiRoot: string = "http://localhost:3000/static_pages/help";
   // private apiRoot1: string = "http://localhost:3000/rainfalls";
-  private apiRoot1: string = "http://bihar.aaupf.org//rainfalls";
+  private apiRoot1: string = "http://bihar.aaupf.org/rainfalls";
   apiRoot: string = environment.apiUrl;
 
-
- 
   test(a,views,rain_fall_type,year,districts) {
     let url = `${this.apiRoot}/rainfalls/test?views=` + views +'&rain_fall_type='+rain_fall_type+'&year='+year;   
     // title;
@@ -29,18 +29,7 @@ export class SvgService {
     this.http.get(url).
     subscribe(res => {
       this.j = res;
-      console.log(url);
-      
-      console.log(res);
-      
-      this.spinner.hide();
-      if (districts == "All") {
-        this.map_districts(this.j) 
-        
-      } else {
-        this.map_bihar_vs_districts(districts)
-      }
-       
+      f.testy(this,districts,rain_fall_type)      
        },
        err => {
          console.log("Error occured.")
@@ -52,237 +41,13 @@ export class SvgService {
   }
 
 
-  map_bihar_vs_districts(l) {
+  map_bihar_vs_districts(l,rain_fall_type) {
 
-    let map = document.getElementById("biharsvg") as HTMLObjectElement;
-    let svgDoc = map.contentDocument; // should be fine
-    let de = svgDoc.querySelectorAll(".fil0");
-    let red:any
-    red = "Red"
+    f.map_b_vs_d(this,l,rain_fall_type)
 
-    var i;
-    // // for (i = 0; i < de.length; i++) {
-    // //     x[i].classList.remove(yellow);
-    // // }
-  
-    for (i = 0; i < de.length; i++) {
-       console.log(de[i].classList.item(0)); 
-      // console.log(i);
-       de[i].classList.remove("Red","Yellow","Orange","Green","Dark_Green","light_green","Lighter_yellow");
-    }
-    this.color_map(de,red)
-
-
-    var i;
-    for (i = 0; i < de.length; i++) {
-
-      // console.log(de[i].getAttribute("id") + "Jila");
-      // console.log(l + "Jilas");
-
-      var m =  l.toLowerCase();
-
-        if (de[i].getAttribute("id") == m) {
-          de[i].classList.add("Orange");
-        } else {
-          de[i].classList.add("Red"); 
-        }
-    }
   }
-
   map_districts(l) {
-    let map = document.getElementById("biharsvg") as HTMLObjectElement;
-    let svgDoc = map.contentDocument; // should be fine
-    // let District = svgDoc.getElementById(a);
-    var dis = ""
-    var dis_y = ""
-    var dis_w = ""
-    var dis_z = ""
-    var dis_e = ""
-    var dis_ex = ""
-    var dis_abov_ext = ""
-
-    console.log(l);
-    for (var k in l){
-      if (l.hasOwnProperty(k)) {
-           if (l[k].below_min) {
-            for (let index = 0; index < l[k].below_min.length; index++) {
-              const element = l[k].below_min[index];
-              //  console.log(element.label+ "Min");
-             var string =  element.label.toLowerCase();
-                if (index == l[k].below_min.length - 1) {
-                  dis += "#"+string+"";
-                } else {
-                  dis += "#"+string+",";
-                }
-            }
-             
-           } 
-           else if (l[k].min) {
-            for (let index = 0; index < l[k].min.length; index++) {
-              const element = l[k].min[index];
-              
-
-              if (element.label == "W. Champaran") {
-                var string:any = "wchamparan";
-
-              } else {
-                var string:any =  element.label.toLowerCase();
-              }
-
-              if (index == l[k].min.length- 1) {
-                dis_y += "#"+string+"";
-              } else {
-                dis_y += "#"+string+",";
-              }
-          
-            }
-          }
-
-          else if (l[k].blow_max) {
-            for (let index = 0; index < l[k].blow_max.length; index++) {
-              const element = l[k].blow_max[index];
-
-              if (element.label == "E. Champaran") {
-                var string:any = "echamparan";
-
-              } else {
-                var string:any =  element.label.toLowerCase();
-              }
-
-              if (index == l[k].blow_max.length- 1) {
-                dis_w += "#"+string+"";
-              } else {
-                dis_w += "#"+string+",";
-              }
-            }
-          }
-
-          else if (l[k].max) {
-            for (let index = 0; index < l[k].max.length; index++) {
-              const element = l[k].max[index];
-
-              var string:any =  element.label.toLowerCase();
-              if (index == l[k].max.length- 1) {
-                dis_z += "#"+string+"";
-              } else {
-                dis_z += "#"+string+",";
-              }
-            }
-          }
-
-          else if (l[k].above_max) {
-            for (let index = 0; index < l[k].above_max.length; index++) {
-              const element = l[k].above_max[index];
-
-              var string:any =  element.label.toLowerCase();
-              if (index == l[k].above_max.length- 1) {
-                dis_e += "#"+string+"";
-              } else {
-                dis_e += "#"+string+",";
-              }
-            }
-          }
-
-
-          else if (l[k].extreme) {
-            for (let index = 0; index < l[k].extreme.length; index++) {
-              const element = l[k].extreme[index];
-
-              var string:any =  element.label.toLowerCase();
-              if (index == l[k].extreme.length- 1) {
-                dis_ex += "#"+string+"";
-              } else {
-                dis_ex += "#"+string+",";
-              }
-            }
-          }
-
-          else if (l[k].above_extreme) {
-            for (let index = 0; index < l[k].above_extreme.length; index++) {
-              const element = l[k].above_extreme[index];
-
-              var string:any =  element.label.toLowerCase();
-              if (index == l[k].above_extreme.length- 1) {
-                dis_abov_ext += "#"+string+"";
-              } else {
-                dis_abov_ext += "#"+string+",";
-              }
-            }
-          }
-           
-           else {
-             
-           }
-      }
-  }
-  let de = svgDoc.querySelectorAll(".fil0");
-  let x = svgDoc.querySelectorAll(dis);
-  let y = svgDoc.querySelectorAll(dis_y);
-  let w = svgDoc.querySelectorAll(dis_w);
-  let z = svgDoc.querySelectorAll(dis_z);
-  let e = svgDoc.querySelectorAll(dis_e);
-  let f = svgDoc.querySelectorAll(dis_ex);
-  let g  = svgDoc.querySelectorAll(dis_abov_ext);
-
-
-
-  // var i;
-  // for (i = 0; i < x.length; i++) {
-  //     x[i].classList.add("mystyle");
-  // }
-  // console.log(dis);
-
-  // console.log(dis_y + "Second");
-  // console.log(dis_w) + "Third";
-  // console.log(dis_z) + "Forth";
-
-  var red:any
-  var orange:any
-  var yellow:any
-  var dark_green:any
-  var green:any
-  var light_green:any
-  var Lighter_yellow:any;
-  red = "Red"
-  orange = "Orange"
-  yellow = "Yellow"
-  dark_green = "Dark_Green"
-  green = "Green"
-  light_green = "light_green"
-  Lighter_yellow = "Lighter_yellow"
-  var i;
-  // // for (i = 0; i < de.length; i++) {
-  // //     x[i].classList.remove(yellow);
-  // // }
-  for (i = 0; i < de.length; i++) {
-     console.log(de[i].classList.item(0)); 
-    // console.log(i);
-     de[i].classList.remove("Red","Yellow","Orange","Green","Dark_Green","light_green","Lighter_yellow");
-  }
-
-  // this.color_map(x,red)
-  // this.color_map(y,orange)
-  // this.color_map(w,yellow)
-  // this.color_map(z,light_green)
-  // this.color_map(e,Lighter_yellow)
-  // this.color_map(f,green) 
-  // this.color_map(g,dark_green) 
-
-
-
-  this.color_map(x,dark_green)
-  this.color_map(y,green)
-  this.color_map(w,light_green)
-  this.color_map(z,Lighter_yellow)
-  this.color_map(e,yellow)
-  this.color_map(f,orange) 
-  this.color_map(g,red) 
-
-
-
-  // this.color_map(y,color)
-
-
+    f.map_all(this,l)
 }
 color_map(x,color) {
   // let x = svgDoc.querySelectorAll(dis);
@@ -300,19 +65,12 @@ color_map(x,color) {
       var svgDoc = a.contentDocument;
       var wchamparan = svgDoc.getElementById("wchamparan");
       let de = svgDoc.getElementsByClassName("fil0");
-
-      console.log(de);
-
       var modal = document.getElementById('myModal');
       var wchamparan1 = svgDoc.getElementById("wchamparan").getAttribute("id");
       var span = document.getElementById("close");
-      
       span.onclick = function() {
         modal.style.display = "none";
        }
-   
-
-
     // de.onclick = function(){
     //   alert("ok");
     //   var id = de.getElementById(this);
